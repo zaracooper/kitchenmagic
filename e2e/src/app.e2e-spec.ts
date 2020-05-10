907a2ad4a70532a8bdc1ef68f1164efa6c6b22ab
Zara Cooper <zramcooper@gmail.com>
2020-05-10 17:22:45 +0300

    Change project name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('angular-github app is running!');
+    expect(page.getTitleText()).toEqual('kitchenmagic app is running!');
   });
 
   afterEach(async () => {
