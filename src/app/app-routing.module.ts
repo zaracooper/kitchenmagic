6b8f1fb420064e103a0b383f23812433ea9b71b1
Zara Cooper <zramcooper@gmail.com>
2020-05-11 10:18:35 +0300

    Add routing module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LostComponent } from './lost/lost.component';
+
+
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: '404', component: LostComponent },
+    { path: '**', redirectTo: '404', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { 
+    anchorScrolling: 'enabled',
+    scrollPositionRestoration: 'top'
+  })],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
