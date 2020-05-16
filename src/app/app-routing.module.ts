3a5200da773ade842923d2b72481fe877401ff53
Zara Cooper <zramcooper@gmail.com>
2020-05-16 18:47:58 +0300

    Add Angular Material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashComponent } from './dash/dash.component';
 import { HomeComponent } from './home/home.component';
 import { LostComponent } from './lost/lost.component';
 
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'me', component: HomeComponent },
+    { path: 'dash', component: DashComponent },
     { path: '404', component: LostComponent },
     { path: '**', redirectTo: '/404'}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,14 @@ import { FooterComponent } from './footer/footer.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { LostComponent } from './lost/lost.component';
 import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { DashComponent } from './dash/dash.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
   declarations: [
@@ -16,11 +24,19 @@ import { HomeComponent } from './home/home.component';
     FooterComponent,
     WelcomeComponent,
     LostComponent,
-    HomeComponent
+    HomeComponent,
+    DashComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatGridListModule,
+    MatCardModule,
+    LayoutModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
